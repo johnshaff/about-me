0252f271104a177cc50495742ad6c85df41665ac
JOHN SHAFF <JOHNSHAFF@GMAIL.COM>
2016-05-10 14:19:46 -0700

    changed html skeleton, began css, and completed first java if statement

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-// This is a comment
+// we are declaring the
+
+var userName = prompt('Please tell write your name below');
+alert('Hi ' + userName);
+console.log('the user\'s name is ' + userName);
+
+// First question
+
+var response1 = prompt('Insert first question here').toLowCase();
+var correctAnswer1 = 'yes';
+
+if (correctAnswer1 === response1 || correctAnswer1[0] === response1) {
+  alert('Great job ' + userName + ' that is correct');
+}
+else {
+  alert('Sorry ' + userName + 'that is incorrect');
+}
+
+// Second question
